3d2f5c3fe79157e0e06014809bf6f5f12ba741c4
Adam Thieme <adam.thieme@gmail.com>
2024-11-23 16:22:14 -0800

    more updates

--- try.sh
+++ try.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+#
+cd $1 || exit
+
+PRE_PULL_HASH=$(git rev-parse HEAD)
+git pull -q
+POST_PULL_HASH=$(git rev-parse HEAD)
+
+if [ "$PRE_PULL_HASH" != "$POST_PULL_HASH" ]; then
+	echo "got update from github"
+fi
+
+
